0f1e278e196541edef0e45a84cf9fdbded710776
portuna85 <portuna85@gmail.com>
2022-07-13 15:39:36 +0900

    JPA 활용

--- src/main/java/com/inven/domain/item/Item.java
+++ src/main/java/com/inven/domain/item/Item.java
@@ -15,23 +15,22 @@ import java.util.List;
 @Getter @Setter
 public abstract class Item {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "item_id")
     private Long id;
 
     private String name;
-
     private int price;
-
     private int stockQuantity;
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //==비즈니스 로직==//
     /**
      * stock 증가
      */
-
     public void addStock(int quantity) {
         this.stockQuantity += quantity;
     }
@@ -39,10 +38,8 @@ public abstract class Item {
     /**
      * stock 감소
      */
-
     public void removeStock(int quantity) {
-        int restStock = stockQuantity -= quantity;
-
+        int restStock = this.stockQuantity - quantity;
         if (restStock < 0) {
             throw new NotEnoughStockException("need more stock");
         }
